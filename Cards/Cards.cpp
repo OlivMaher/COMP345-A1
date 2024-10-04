561a303f9798046b4f136dbd69c80d599ddbb466
Karanvir Bassi <karanvirbassi514@gmail.com>
2024-10-04 18:55:10 -0400

    Update Cards.cpp

--- Cards/Cards.cpp
+++ Cards/Cards.cpp
@@ -18,7 +18,7 @@ void Card::play(Deck& deck, Hand& hand) { //uses a card from a hand, returns it 
 
     while (useCard){ // loops until turn is over
         
-        if (hand.totalCardsHand() <=0){ // case if hand is empty
+        if (hand.getHand() <=0){ // case if hand is empty
         cout << "No Cards to play.\n\n";
         useCard = false;
         }
@@ -39,7 +39,7 @@ void Card::play(Deck& deck, Hand& hand) { //uses a card from a hand, returns it 
                 cout << "Which card would you like to use?\n";
                 int choice; // which card should be played
                 cin >> choice;
-                if (choice < 0 || choice > hand.totalCardsHand()){ // checks for valid card number
+                if (choice < 0 || choice > hand.getHand()){ // checks for valid card number
                     cout << "Invalid Card.\n\n";
                 }
                 else{ // if valid card number is given
@@ -87,51 +87,51 @@ Deck::Deck() { //  Default constructor for deck
 }
 
 Card Deck::draw(){
-    if (totalCardsDeck() <= 0){ // prevents a draw if the deck is empty
+    if (getDeck() <= 0){ // prevents a draw if the deck is empty
         cout << "No cards in deck.\n" << "\n";
         return Card();
     }
     else{
         srand(time(0)); // used for random draw form deck
         int ran = rand();
-        int take = ran % totalCardsDeck();
+        int take = ran % getDeck();
         cout << ran << " " << take << "\n";
         Card toRemove(cards[take]); 
 
-        cards[take] = cards[totalCardsDeck()-1]; // places the last card in tha array to the position of the removed card
-        cards[totalCardsDeck()-1] = NULL;
+        cards[take] = cards[getDeck()-1]; // places the last card in tha array to the position of the removed card
+        cards[getDeck()-1] = NULL;
 
-        int newSize = totalCardsDeck()-1;
-        updateDeck(newSize); // updates the number of cards in deck
+        int newSize = getDeck()-1;
+        setDeck(newSize); // updates the number of cards in deck
         
         return toRemove; // returns the card that was removed from the deck
     }
 }
 
 void Deck::returnToDeck(Card& card) { // adds a card to the end of the deck
-    cards[totalCardsDeck()] = card;
-    updateDeck(totalCardsDeck() + 1);
+    cards[getDeck()] = card;
+    setDeck(getDeck() + 1);
 }
 
 void Deck::showDeck(){ // displays the deck
-    if (this->totalCardsDeck() == 0){
+    if (this->getDeck() == 0){
          cout << "Mo cards in Deck.\n";
     }
     else{    
         cout << "Deck List\n";
         cout << "---------------------\n";
-        for (int i=0;i<(this->totalCardsDeck());i++){
+        for (int i=0;i<(this->getDeck());i++){
             cout << (i+1)<< ".  "<< cards[i].getCardType() <<"\n";
         }
         cout << "\n";
     }
 }
 
-int Deck::totalCardsDeck(){ // returns the numbers of cards currently in the deck
+int Deck::getDeck(){ // returns the numbers of cards currently in the deck
     return cardsInDeck;
 }
 
-void Deck::updateDeck(int a){ // used to change the number of cards in the deck
+void Deck::setDeck(int a){ // used to change the number of cards in the deck
     cardsInDeck =a;
 }
 
@@ -141,43 +141,43 @@ Hand::Hand() {// default constructor for hand
 }
 
 void Hand::takeCard(Deck& deck){
-    if (totalCardsHand() >= 20){
+    if (getHand() >= 20){
         cout << "All cards in hand.\n\n";
     }
-    if (deck.totalCardsDeck() <= 0) {
+    if (deck.getDeck() <= 0) {
     cout << "No cards available in deck.\n\n";
     }
     else{
         Card take = deck.draw(); // draws a card from deck
-        hand[totalCardsHand()] = take; // adds the card to the hand
-        updateHand(totalCardsHand()+1); // adjusts the size of the hand
+        hand[getHand()] = take; // adds the card to the hand
+        setHand(getHand()+1); // adjusts the size of the hand
     }
 }
 
 void Hand::showHand(){ // displays the hand
-    if (this->totalCardsHand() == 0){
+    if (this->getHand() == 0){
          cout << "No cards in Hand.\n\n";
     }
     else{    
         cout << "Current Hand\n";
         cout << "---------------------\n";
-        for (int i=0;i<(this->totalCardsHand());i++){
+        for (int i=0;i<(this->getHand());i++){
             cout << (i+1)<< ".  "<< hand[i].getCardType() <<"\n";
         }
         cout << "\n";
     }
 }
 
-int Hand::totalCardsHand(){ // returns the numbers of cards currently in a hand
+int Hand::getHand(){ // returns the numbers of cards currently in a hand
     return cardsInHand;
 }
 
-void Hand::updateHand(int a){ // used to change the number of cards in a hand
+void Hand::setHand(int a){ // used to change the number of cards in a hand
     cardsInHand = a;
 }
 
 Card Hand::handToDeck(int a,Deck& deck){ // returns a card that was removed from a hand, but returned to the deck
-    if ((a < 0 || a >= totalCardsHand())){
+    if ((a < 0 || a >= getHand())){
         //cout << totalCardsHand();
         cout << "Invalid card number.\n\n";
         return Card();
@@ -185,11 +185,11 @@ Card Hand::handToDeck(int a,Deck& deck){ // returns a card that was removed from
     else{
         int index = a;
         Card toRemove(hand[index]);
-        hand[index] = hand[totalCardsHand()-1];// positions the last card in the hand to the position of the removed card
-        hand[totalCardsHand()-1] = NULL;
+        hand[index] = hand[getHand()-1];// positions the last card in the hand to the position of the removed card
+        hand[getHand()-1] = NULL;
 
-        int newSize = totalCardsHand()-1;
-        updateHand(newSize); // adjusts the number of cards
+        int newSize = getHand()-1;
+        setHand(newSize); // adjusts the number of cards
         deck.returnToDeck(toRemove); // returns card to deck
         
         return toRemove;
@@ -197,7 +197,7 @@ Card Hand::handToDeck(int a,Deck& deck){ // returns a card that was removed from
 }
 
 Card Hand::copyCard_Hand(int a){ // returns a copy of card from a hand based on the position of the card
-    if (a<0 || a >= totalCardsHand()){
+    if (a<0 || a >= getHand()){
         cout << "Nothing to return\n\n";
         return Card();
     }
